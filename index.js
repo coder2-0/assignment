ef4d4283e40e18b70e14020b32a6a162feb4929c
coder2-0 <143971946+coder2-0@users.noreply.github.com>
2024-03-26 00:03:12 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const username = loginForm.username.value.trim();
+        const password = loginForm.password.value; 
+
+        localStorage.setItem('username', username);
+
+        
+        if (username.endsWith('@worker.com')) {
+            window.location.href = 'cowokers.html'; 
+        } else if (username.endsWith('@owner.com')) {
+            window.location.href = 'owner.html'; 
+        } else {
+            alert('Please use a valid email address.');
+        }
+    });
+
+    const signupLink = document.getElementById('signupLink');
+    signupLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        window.location.href = 'signup.html'; 
+    });
+});
